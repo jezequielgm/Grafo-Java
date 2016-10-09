e681cb0a31ee024abeb9d9689fc0a41b6908c0cb
Zeku <nocomment182@gmail.com>
2016-10-09 16:49:11 +0200

    codigo fuente

---
+++ Grafo.java
@@ -0,0 +1,198 @@
+import java.util.*;
+
+/* 
+ * Esta clase modela y gestiona un grafo no dirigido
+ * usando una estructura de listas para representar
+ * las adyacencias. Además contiene métodos para
+ * gestionar vértices y aristas.
+ * Los vértices se distinguen por su etiqueta.
+ * Un par de vértices solo permite una arista entre ellos
+ */
+
+public class Grafo{
+
+    private HashMap<String, Vertice> vertices;
+    private HashMap<Integer, Arista> aristas;
+
+    /**
+     * Construcción de un grafo vacío
+     **/
+    public Grafo()
+    {
+	this.vertices = new HashMap<String, Vertice>();
+	this.aristas = new HashMap<Integer, Arista>();
+    }
+
+
+    /**
+     * Construcción de un grafo que acepta una lista
+     * de vertices por parámetro de entrada
+     *
+     *@param vertices. Array de lista usado para llenar el grafo
+     **/
+    public Grafo(ArrayList<Vertice> vertices)
+    {
+	this.vertices = new HashMap<String, Vertice>();
+	this.aristas = new HashMap<Integer, Arista>();
+
+	for(Vertice v : vertices )
+	    {
+		this.vertices.put(v.getEtiqueta(), v);
+	    }
+	
+    }
+
+
+    /**
+     * Inserta una arista unitaria entre los vertices v1 y v2
+     * si y solo si no exista ya una arista que los una
+     *
+     * @param v1. Un extremo de la arista
+     * @param v2. Otro extremo de la arista
+     * @return true. Si y solo si la arista no existe previamente
+     **/
+    public boolean insertarArista(Vertice v1, Vertice v2)
+    {
+      	return insertarArista(v1, v2, 1);
+    }
+
+    
+    /**
+     * Inserta una arista entre los vertices v1 y v2
+     * y un coste o peso especificado por parámetro de entrada. 
+     * La arista se insertará con éxito siempre que sea única y además
+     * no haya otra uniendo actualmente v1 y v2. Finalmente v1 y v2 no
+     * deben ser el mismo vértice (v1 != v2)
+     *
+     * @param v1. Un extremo de la arista
+     * @param v2. Otro extremo de la arista
+     * @param peso. Coste para llegar de v1 a v2 o viceversa
+     * @return true. Si y solo si la arista no existe previamente
+     **/
+    public boolean insertarArista(Vertice v1, Vertice v2, int peso)
+    {
+	if(v1.equals(v2)) //vertices identicos?
+	    return false;
+
+	Arista arista = new Arista(v1, v2, peso);
+
+	if(aristas.containsKey(arista.hashCode())) //arista ya está en el grafo?
+	    return false;
+	else if( v1.contieneUnVecino(arista) || v2.contieneUnVecino(arista)) //arista ya une a v1 o v2?
+	    return false;
+
+	aristas.put(arista.hashCode(), arista);
+	v1.insertarVecino(arista);
+	v2.insertarVecino(arista);
+	return true;
+    }
+
+    /** 
+     * @param arista. Arista que estamos buscando
+     * @return true. Si y solo si el grafo contiene a la arista
+     **/
+    public boolean contieneLaArista(Arista arista)
+    {
+	if(arista.getVetice1() == null || arista.getVertice2() == null)
+	    return false;
+	return this.aristas.containsKey(arista.hashCode());
+    }
+
+
+    /**
+     * Elimina una arista del grafo. Por tanto, los vertices que unían
+     * pierden la adyacencia entre ellos
+     *
+     *@param arista. Arista que se quiere eliminar del grafo
+     *@return Arista. Arista borrada del grafo
+     */
+    public Arista eliminarArista(Arista arista)
+    {
+	arista.getVertice1().eliminarVecino(arista);
+	arista.getVertice2().eliminarVecino(arista);
+	return this.aristas.remove(arista.hashCode());
+    }
+
+    /**
+     * Nos devuelve true si encuentra el vértice que se pasa
+     * como parámetro de entrada
+     * 
+     * @param vertice. Vértice que buscamos
+     * @return boolean. True si el vertice se encuentra.
+     **/    
+    public boolean contieneElVertice(Vertice vertice)
+    {
+	return (this.vertices.get(vertice.getEtiqueta()) != null);
+    }
+
+    /**
+     * @param etiqueta. Distintivo de cada vértice
+     * @return Vertice. Devuelve el vértice asociado a la etiqueta
+     **/
+    public Vertice getVertice(String etiqueta)
+    {
+	return this.vertices.get(etiqueta);
+    }
+
+    /**
+     * Inserta un nuevo vértice. Si el vértice existe previamente entonces
+     * se consulta si puede ser sobreescrito. En tal caso se elimina las adyacencias
+     * existentes.
+     *
+     * @param vertice. Vértice a insertar
+     * @param sobreescribeVertice. Permiso para sobreescribir el vértice
+     * @return boolean. Verdarero si el vértice se inserta con éxito
+     **/
+    public boolean insertarVertice(Vertice vertice, boolean sobreescribeVertice)
+    {
+	Vertice actual = this.vertices.get(vertice.getEtiqueta);
+	if(actual != null) //existía previamente?
+	    {
+		if(!sobreescribeVertice)
+		    return false;
+
+		while(actual.getContarVecinos() >= 0)
+		    this.eliminarArista(actual.getVecino(0));
+		
+	    }
+
+	vertices.put(vertice.getEtiqueta, vertice);
+	return true;
+    }
+
+    /**
+     * Elimina el vértice especificado mediante la etiqueta
+     * distintiva por parámetro de entrada. Al eliminar el vértice
+     * se elimina también todas las adyancencias que poseía este.
+     *
+     * @param etiqueta. Cadena distintiva de cada vértice
+     * @return Vertice. Devuelve el vértice eliminado
+     **/
+    public Vertice eliminarVertice(String etiqueta)
+    {
+	Vertice vertice = vertices.remove(etiqueta);
+
+	while(vertice.getContarVecinos() >= 0)
+	    this.eliminarArista(vertice.getVecino(0));
+
+	return vertice;
+    }
+
+    /**
+     * @return Set<String>. Devuelve las etiquetas, que son el distintivo
+     * único de cada objeto Vertice en el Grafo
+     **/
+    public Set<String> verticeKeys()
+    {
+	return this.vertices.keySet();
+    }
+
+    /**
+     * @return Set<Arista>. Devuelve todos los objetos Arista del Grafo
+     **/
+    public Set<Arista> getAristas()
+    {
+	return new HashSet<Arista>(this.aristas.values());
+    }
+    
+}
---
+++ Vertice.java
@@ -0,0 +1,133 @@
+import java.util.ArrayList;
+
+/*
+ * Cada instancia a esta clase representa cada vertice
+ * Cada vertice tiene asociado una etiqueta
+ * Por tanto, la etiqueta tiene un contenido único
+ * El array de lista vecindad contiene las aristas incidentes al vértice
+*/
+
+
+public class Vertice
+{
+ 
+    private ArrayList<Arista> vecindad;
+    private String etiqueta;
+
+    public Vertice (String etiqueta)
+    {
+	this.etiqueta = etiqueta;
+	this.vecindad = new ArrayList<Arista>();
+    }
+
+    /**
+     * Añade un objeto Arista al array de lista vecindad
+     * si y solo este no esté contenido en dicho array de lista 
+     * @param arista. Objeto a añadir
+     */
+    public void insertarVecino(Arista arista)
+    {
+	if( !this.vecindad.contains(arista))
+	    this.vecindad.add(arista);
+    }
+
+    /**
+     * Comprueba si la arista incide en este vertice
+     * @param arista. Objeto a evaluar
+     * @return true. Si y solo si el objeto esta contenido en el array de lista vecindad
+     */
+    public boolean contieneUnVecino(Arista arista)
+    {
+	return this.vecindad.contains(arista)
+    }
+
+    /**
+     * @param indice. Indica la posicion a extraer
+     * @return Arista. La arista apuntada por el índice en el array de lista vecindad
+     */
+    public Arista getVecino(int indice)
+    {
+	return this.vecindad.get(indice);
+    }
+
+    /**
+     * Se elimina una arista del array de lista vecindad mediante posición
+     * @param indice. Indica la posicion a eliminar
+     * @return Arista. La Arista que ha sido eliminada del array de lista vecindad
+     */
+    public Arista eliminarVecino(int indice)
+    {
+	return this.vecindad.remove(indice);
+    }
+
+
+    /**
+     * Se elimina el objeto Arista del array de lista vecindad mediante
+     * el identificando la referencia del objeto Arista
+     * @param arista. indice el objeto Arista a eliminar
+     */
+    public void eliminarVecino(Arista arista)
+    {
+	this.vecindad.remove(arista);
+    }
+
+    /**
+     * @return int. Se devuelve el número de aristas incidentes (o vecinos) 
+     * tiene el vértice en el array de lista vecindad
+     */
+    public int getContarVecinos()
+    {
+	return this.vecindad.size();
+    }
+
+    /**
+     * @return String. Devuelve el valor de la cadena etiqueta
+     */
+    public String getEtiqueta()
+    {
+	return this.etiqueta;
+    }
+
+    /**
+     * Se comprueba si vertice2 es un objeto de tipo Vertice
+     * En tal caso podemos convertirlo de Object a Vertice
+     * Y por último evaluamos si contiene el mismo valor que el objeto vertice actual
+     * Para ello las etiquetas deben coincidir(son únicas para cada objeto Vertice) 
+     * @param vertice2. Objeto que comparamos con el vertice para 
+     * evaluar si son el mismo objeto
+     * @return true. Efectivamente son el mismo objeto
+     */
+    public boolean equals(Object vertice2)
+    {
+	if( !vertice2 instanceof Vertice)
+	    return false;
+
+	Vertice v = (Vertice) vertice2;
+	return this.etiqueta.equals(v.etiqueta);
+    }
+
+    /**
+     * @return String. Representación del vértice en una cadena
+     */
+    public String toString()
+    {
+	return "Vertice: " + this.etiqueta;
+    }
+
+
+    /**
+     * @return int. Código hash para este vértice
+     **/
+    public int hashCode()
+    {
+	return this.getEtiqueta().hashCode();
+    }
+    
+    /**
+     * @return ArrayList<Arista>. Copia del array de lista vecindad
+     */ 
+    public ArrayList<Arista> getVecinos()
+    {
+	return new ArrayList<Arista>(this.vecindad);
+    }
+}
